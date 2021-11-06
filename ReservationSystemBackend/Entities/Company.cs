d8f472898a68b47e7fabf5cf9d8e00a32b0e47de
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-06 20:57:19 +0200

    Add project files.

---
+++ ReservationSystemBackend/Entities/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystemBackend.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
