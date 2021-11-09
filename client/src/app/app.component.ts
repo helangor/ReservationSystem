c39f20e5734e827711282c211c4b3d8201b96dc0
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-09 23:06:04 +0200

    Added getCompanies

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users: any;
+
+constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getCompanies();
+  }
+
+
+  getCompanies() {
+    this.http.get('https://localhost:44383/api/Companies').subscribe( res => {
+      this.users = res
+    }, error => console.log("error")
+    )
+  }
 }
