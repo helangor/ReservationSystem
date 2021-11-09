2bec5ca520f23c89984ea8010443781e7d584409
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-09 19:01:27 +0200

    Added CompaniesController

---
+++ ReservationSystem/Controllers/CompaniesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReservationSystemBackend.Data;
+using ReservationSystemBackend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CompaniesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Company>> GetCompanies()
+        {
+            var companies = _context.Companies.ToList();
+
+            return companies;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Company> GetCompany(int id)
+        {
+            var company = _context.Companies.Find(id);
+
+            return company;
+        }
+    }
+}
