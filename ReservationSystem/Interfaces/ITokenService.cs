43a196ce17c0309a7a60bfe4d950dd3b3f9be701
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-12 20:19:20 +0200

    Added TokenService

---
+++ ReservationSystem/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using ReservationSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(User user);
+    }
+}
--- ReservationSystem/Startup.cs
+++ ReservationSystem/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ReservationSystem.Interfaces;
+using ReservationSystem.Services;
 using ReservationSystemBackend.Data;
 using System;
 using System.Collections.Generic;
@@ -27,6 +29,7 @@ namespace ReservationSystemBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
