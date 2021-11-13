f95ec80edd4586b892822531eac5e44182da2fa8
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-13 20:22:45 +0200

    Added SetCurrentUser

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,18 @@ export class AppComponent implements OnInit {
   title = 'client';
   companies: any;
 
-constructor(private http: HttpClient) {}
+constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit() {
     this.getCompanies();
+    this.setCurrentUser();
   }
 
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
 
   getCompanies() {
     this.http.get('https://localhost:44383/api/Companies').subscribe( res => {
