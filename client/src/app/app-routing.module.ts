c32a70738f7c5eacd1280c0523a4c6319c180e89
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-18 22:24:54 +0200

    Added first routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'admin', component: AdminPanelComponent},
+  {path:'company/:id', component: CompanyDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
