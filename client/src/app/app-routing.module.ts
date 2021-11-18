07808c43176aea33c540caa74cf26cf919eb99cc
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-18 22:56:39 +0200

    Added routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { CompanyAdminPanelComponent } from './company-admin-panel/company-admin-panel.component';
 import { CompanyDetailComponent } from './company-detail/company-detail.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'admin', component: AdminPanelComponent},
   {path:'login', component: LoginComponent},
+  {path:'company-admin', component: CompanyAdminPanelComponent},
   {path:'company/:id', component: CompanyDetailComponent},
   {path:'**', component: HomeComponent, pathMatch: 'full'},
 ];
