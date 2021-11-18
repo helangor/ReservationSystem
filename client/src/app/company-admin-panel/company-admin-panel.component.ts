feaee42b4528b7d23d27ea7c8785a0be59abd3af
Henrikki Helander <henrikki_helander@hotmail.com>
2021-11-18 22:55:53 +0200

    Added company-admin-panel

---
+++ client/src/app/company-admin-panel/company-admin-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-admin-panel',
+  templateUrl: './company-admin-panel.component.html',
+  styleUrls: ['./company-admin-panel.component.scss']
+})
+export class CompanyAdminPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
