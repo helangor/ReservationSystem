95b6be52cacf18e30d02c625abc47f2f93ff1eab
Henrikki Helander <henrikki_helander@hotmail.com>
2021-12-12 22:51:51 +0200

    Updated calendar

--- client/src/app/company-detailed/company-detailed.component.ts
+++ client/src/app/company-detailed/company-detailed.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { DateRange, DefaultMatCalendarRangeStrategy, MAT_DATE_RANGE_SELECTION_STRATEGY } from '@angular/material/datepicker';
+import { DateRange } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { Company } from '../models/company';
+import { ReservationDialogComponent } from '../reservation-dialog/reservation-dialog.component';
 import { CompanyService } from '../services/company.service';
 
 @Component({
@@ -13,16 +14,12 @@ import { CompanyService } from '../services/company.service';
 export class CompanyDetailedComponent implements OnInit {
   company: Company;
   selectedDateRange: DateRange<Date>;
-
-  myFilter = (d: Date | null): boolean => {
-    const day = (d || new Date()).getDay();
-    // Prevent Saturday and Sunday from being selected.
-    return day !== 0 && day !== 6;
-  };
-
-  //TODO: Filtteri mennyttä päivää ei voi valita. Suomen kielelle kuukaudet. Inline kalenteri.
   
-  constructor(private companyService: CompanyService, private route: ActivatedRoute) { }
+  constructor(
+    private companyService: CompanyService, 
+    private route: ActivatedRoute,
+    public dialog: MatDialog) { }
+
 
   ngOnInit(): void {
     this.loadCompany();
@@ -30,13 +27,13 @@ export class CompanyDetailedComponent implements OnInit {
 
   loadCompany() {
     this.companyService.getCompany(this.route.snapshot.paramMap.get('companyName')).subscribe(company => {
-      console.log("COMP ", company);
       this.company = company;
     })
   }
 
 
   _onSelectedChange(date: Date): void {
+    //TODO: tähän saa laitettua, että jos välissä varattu päivä niin ei voi valita.
     if (
       this.selectedDateRange &&
       this.selectedDateRange.start &&
@@ -45,6 +42,7 @@ export class CompanyDetailedComponent implements OnInit {
     ) {
       this.selectedDateRange = new DateRange(
         this.selectedDateRange.start,
+
         date
       );
     } else {
@@ -52,4 +50,48 @@ export class CompanyDetailedComponent implements OnInit {
     }
   }
 
+  reserve() {
+    if (!this.selectedDateRange) return;
+
+    if (this.selectedDateRange.start == null || this.selectedDateRange.end == null) return;
+
+    this.openReservationDialog();
+    console.log("RESERVE ", this.selectedDateRange);
+
+  }
+
+
+  myDateFilter = (d: any) => {
+    let calendarDate = d.toDate();
+    let currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() - 18);
+
+    let day = new Date('2021-12-13T03:24:00');
+    if (this.sameDay(day, calendarDate)) return false;
+    //TODO: Tässä filtteröi pois päivät, jotka varattu. NgOninitissä hakee varatut päivät ja sitten tässä check löytyykö.
+
+    return calendarDate > currentDate;
+  }
+
+  sameDay(d1: Date, d2: Date) {
+    return d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate();
+  }
+
+  openReservationDialog() {
+    const dialogRef = this.dialog.open(ReservationDialogComponent);
+
+    this.dialog.open(ReservationDialogComponent, {
+      data: {
+        dateRange: this.selectedDateRange,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  
 }
+
