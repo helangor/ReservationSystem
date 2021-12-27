4d83f4ab0f7ca1c30ef33c41e0bf68622c30f1d1
Henrikki Helander <henrikki_helander@hotmail.com>
2021-12-27 18:53:58 +0200

    Added CompanyController

---
+++ ReservationSystem/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Entities;
+using ReservationSystemBackend.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Controllers
+{
+    public class CompanyController : BaseApiController
+    {
+        private readonly DataContext context;
+        private readonly IMapper mapper;
+
+        public CompanyController(DataContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("GetCompaniesByUserName")]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompaniesByUserName(string username)
+        {
+            var companies = await context.Companies.Where(c => c.Users.Any(u => u.UserName == username.ToLower())).Include(p => p.Products).ToListAsync();
+            return Ok(companies);
+        }
+    }
+}
